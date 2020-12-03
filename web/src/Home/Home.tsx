72e2522082c9814bd3ac2b3d2894a2b46dbfda81
Ron Brogan <ronaldbrogan@hotmail.com>
2020-12-02 21:18:08 -0800

    Fix github link on homepage

--- web/src/Home/Home.tsx
+++ web/src/Home/Home.tsx
@@ -17,7 +17,7 @@ export default class Home extends React.Component {
                     As time goes on, more games' data will be added and more articles will be written/added.
                 </p>
                 <p>
-                    If you'd like to contribute or spot an inaccuracy, this site is on <a href="https://github.com/ronbrogan/OpenBlam">Github</a> - pull requests and issues are welcome.
+                    If you'd like to contribute or spot an inaccuracy, this site is on <a href="https://github.com/ronbrogan/OpenBlam.com">Github</a> - pull requests and issues are welcome.
                 </p>
                 <em>
                     - Ron
